b8f45133b15a212b36c91e98560b4295c11302cb
Ori Wwininger <oriwininger0@gmail.com>
2013-07-18 10:23:56 +0300

    fixed wrong spelling

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,8 @@
 class Application < ActiveRecord::Base
 
-   attr_accessible :attachment, :country, :full_name, :city, :job, :linkdin
-   has_attached_file :attachment, :url  => "/assets/products/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+   attr_accessible :attachment, :full_name, :linkedin, :country, :city, :job
+   has_attached_file :attachment,
+ 		:url  => "/assets/products/:id/:style/:basename.:extension",
+        :path => ":rails_root/public/uploads/:id/:style/:basename.:extension"
+
 end
